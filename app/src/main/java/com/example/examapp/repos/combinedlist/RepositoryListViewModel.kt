7b15c07f898fe00dbfffc349eea767a738dcb593
vlad <mialikvlad@gmail.com>
2022-08-24 17:15:30 +0300

    searchView

--- app/src/main/java/com/example/examapp/repos/combinedlist/RepositoryListViewModel.kt
+++ app/src/main/java/com/example/examapp/repos/combinedlist/RepositoryListViewModel.kt
@@ -9,6 +9,7 @@ import com.example.domain.usecases.GetBitbucketRepositoriesUseCase
 import com.example.domain.usecases.GetGithubRepositoriesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
@@ -19,11 +20,16 @@ class RepositoryListViewModel @Inject constructor(
     val getBitbucketRepositoriesUseCase: GetBitbucketRepositoriesUseCase
 ) : ViewModel() {
 
-    private var constRepoList = mutableListOf<RepositoryModel>()
+    private var responseRepoList = mutableListOf<RepositoryModel>()
+    private val queryFlow = MutableStateFlow("")
 
     private val _listRepo: MutableLiveData<List<RepositoryModel>> = MutableLiveData()
     val listRepo: LiveData<List<RepositoryModel>>
         get() = _listRepo
+
+    private val _successState: MutableLiveData<Boolean> = MutableLiveData(false)
+    val successState: LiveData<Boolean>
+        get() = _successState
 
     init {
         viewModelScope.launch {
@@ -32,29 +38,31 @@ class RepositoryListViewModel @Inject constructor(
     }
 
     private suspend fun initRepos() = runBlocking {
+        responseRepoList = mutableListOf()
         val githubRepoList = async { getGithubRepositoriesUseCase() }
         val bitbucketRepoList = async { getBitbucketRepositoriesUseCase() }
-        constRepoList.addAll(bitbucketRepoList.await())
-        constRepoList.addAll(githubRepoList.await())
-        _listRepo.value = constRepoList
+        responseRepoList.addAll(bitbucketRepoList.await())
+        responseRepoList.addAll(githubRepoList.await())
+        _listRepo.value = responseRepoList
+        _successState.value = true
     }
 
     fun filterOnlyGithubRepos() {
-        val mutableRepoList = constRepoList
+        val mutableRepoList = responseRepoList
         _listRepo.value = mutableRepoList.filter { repo ->
             repo.webService == "Github"
         }
     }
 
     fun filterOnlyBitbucketRepos() {
-        val mutableRepoList = constRepoList
+        val mutableRepoList = responseRepoList
         _listRepo.value = mutableRepoList.filter { repo ->
             repo.webService == "Bitbucket"
         }
     }
 
     fun getLoadedRepos() {
-        _listRepo.value = constRepoList
+        _listRepo.value = responseRepoList
     }
 
     fun sortAlphabetically() {
@@ -67,5 +75,16 @@ class RepositoryListViewModel @Inject constructor(
         _listRepo.value = _listRepo.value?.sortedByDescending { repoModel ->
             repoModel.repositoryName.first().lowercaseChar()
         }
+    }
+
+    fun searchByOwnerName(query: String): Flow<String> {
+        _listRepo.value = _listRepo.value?.filter { repositoryModel ->
+            repositoryModel.ownerName.contains(query)
+        }
+        return flow { }
+    }
+
+    suspend fun reloadRepos() {
+        initRepos()
     }
 }
